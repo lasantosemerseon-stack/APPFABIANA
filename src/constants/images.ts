950fdfb47868fbbe41a466c8a7ed146f3492b55e
lasantosemerseon-stack <lasantosemerseon@gmail.com>
2026-04-13 16:54:54 -0300

    Create images.ts

---
+++ src/constants/images.ts
@@ -0,0 +1,18 @@
+export const LOGO_URL = 'https://customer-assets.emergentagent.com/job_8428dfd2-2530-48ad-8f9b-4f75403d5948/artifacts/57d8mctq_IMG_7871.PNG';
+export const CHALLENGE_HERO = 'https://images.unsplash.com/photo-1768839722667-85bf00fe8b9a?w=800&q=80';
+export const FOOD_IMAGES = { saladBackground: 'https://images.unsplash.com/photo-1654458804670-2f4f26ab3154?w=800&q=80' };
+
+export const MODULE_IMAGES: Record<string, string> = {
+  module_1: 'https://customer-assets.emergentagent.com/job_8428dfd2-2530-48ad-8f9b-4f75403d5948/artifacts/xa7pdmdj_%2B365%20receitas%20ovo.png',
+  module_2: 'https://customer-assets.emergentagent.com/job_receitas-saudaveis-7/artifacts/67thsiqu_CAPA%20150%20RECEITA%20AIR%20FRYER.png',
+  module_3: 'https://customer-assets.emergentagent.com/job_receitas-saudaveis-7/artifacts/co120gqi_BOLOS%20E%20DOCES.png',
+  module_4: 'https://customer-assets.emergentagent.com/job_receitas-saudaveis-7/artifacts/cjglo1o8_Sobremesas%20geladas.png',
+  module_5: 'https://customer-assets.emergentagent.com/job_receitas-saudaveis-7/artifacts/h0vbazr6_padaria%20e%20paes%20zero%20glut%C3%A9n.png',
+  module_6: 'https://customer-assets.emergentagent.com/job_8428dfd2-2530-48ad-8f9b-4f75403d5948/artifacts/rhu50cg4_200%20receitas%20com%20whey%20protein.png',
+  module_7: 'https://customer-assets.emergentagent.com/job_8428dfd2-2530-48ad-8f9b-4f75403d5948/artifacts/3t61yten_450%20receitas%20para%20diabeticos.png',
+  module_8: 'https://customer-assets.emergentagent.com/job_receitas-saudaveis-7/artifacts/0nuhkrn5_lancheira%20kids.png',
+  module_9: 'https://customer-assets.emergentagent.com/job_receitas-saudaveis-7/artifacts/ji0k22jq_sucos%20e%20chas%20detox.png',
+  module_10: 'https://customer-assets.emergentagent.com/job_receitas-saudaveis-7/artifacts/jlskl04z_SOPAS%20E%20CREMES.png',
+};
+
+export const WHATSAPP_NUMBER = '5562993938302';
